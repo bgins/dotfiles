1b8774a68ff02c1670dfc5abfd6cdb528aa81644
Brian Ginsburg <brian@brianginsburg.com>
2016-09-23 08:29:30 -0700

    Add latex, xkcd. Remove games.

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -29,10 +29,10 @@ values."
      emacs-lisp
      emoji
      erc
-     games
      git
      html
      javascript
+     latex
      markdown
      org
      ranger
@@ -44,6 +44,7 @@ values."
      syntax-checking
      themes-megapack
      version-control
+     xkcd
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
@@ -260,6 +261,7 @@ before packages are loaded. If you are unsure, you should try in setting them in
                                "~/Documents/org/gre.org"))
   )
 
+
 (defun dotspacemacs/user-config ()
   "Configuration function for user code.
 This function is called at the very end of Spacemacs initialization after
@@ -267,6 +269,8 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+  ;; full document previews for latex
+  (add-hook 'doc-view-mode-hook 'auto-revert-mode)
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
