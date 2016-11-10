91a1352bcf665aaf28fa58dca5328b9cf983a8b2
thuselem <brian@brianginsburg.com>
2016-11-10 08:21:57 -0800

    Add yaml layer.

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -47,6 +47,7 @@ values."
      themes-megapack
      version-control
      xkcd
+     yaml
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
