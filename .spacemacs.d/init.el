b93f6904ed4299631dd1260c0bcb7894a2dc1c0a
thuselem <brian@brianginsburg.com>
2016-11-20 18:21:06 -0800

    Remove anaconda-mode exclusion.
    Seems to work now.

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -58,8 +58,7 @@ values."
    ;; A list of packages and/or extensions that will not be install and loaded.
    dotspacemacs-excluded-packages
    '(
-     ;; temporary exclusion
-     anaconda-mode
+
    )
    ;; If non-nil spacemacs will delete any orphan packages, i.e. packages that
    ;; are declared in a layer which is not a member of
