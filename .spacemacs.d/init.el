34c3e04de90b3d64a15eb9c87d9067e55cd45282
Brian Ginsburg <brian@brianginsburg.com>
2016-11-29 11:56:30 -0800

    Add --color to magit log.

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -294,6 +294,7 @@ you should place your code here."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(magit-log-arguments (quote ("--graph" "--color" "--decorate" "-n256")))
  '(paradox-github-token t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
