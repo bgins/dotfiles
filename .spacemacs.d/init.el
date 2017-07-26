49c8603ac51fe0a931feda215437d2e3fc2b07e0
Brian Ginsburg <brian@brianginsburg.com>
2017-07-25 21:57:27 -0700

    Choose two good themes

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -118,16 +118,8 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(flatland
-                         soft-charcoal
-                         spacemacs-dark
-                         spacemacs-light
-                         soft-charcoal
-                         solarized-light
-                         solarized-dark
-                         leuven
-                         monokai
-                         zenburn)
+   dotspacemacs-themes '(sanityinc-tomorrow-night
+                         flatland)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font. `powerline-scale' allows to quickly tweak the mode-line
